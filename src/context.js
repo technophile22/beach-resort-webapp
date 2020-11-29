22205c1450998658c32bcb0846b2619ff0886f54
technophile22 <raghavmangal22@gmail.com>
2020-11-29 22:31:04 +0530

    context setup and single room page image dynamic access using styled components

--- src/context.js
+++ src/context.js
@@ -46,9 +46,20 @@ class RoomProvider extends Component {
         return tempItems;
 
     }
+
+    getRoom = slug => {
+        let tempRooms = [...this.state.rooms];
+        const room = tempRooms.find(room => room.slug === slug);
+        
+        return room; //its an object, if we have used filter instead of find, then we will have to get it out of array and then use filter method
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={ { ...this.state }}>
+            <RoomContext.Provider value={{
+                ...this.state,
+                getRoom: this.getRoom
+            }}>
                 {this.props.children}                
             </RoomContext.Provider >
         )
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../context';
+import StyledHero from '../components/StyledHero';
 
 export default class SingleRoom extends Component {
+
+    constructor(props){
+        super(props);
+        //console.log(this.props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        };
+    }
+
+    static contextType = RoomContext;
+
+
+    // componentDidMount() {}
+
     render() {
-        return (
-            <div>
-                Hello from single room page
+        //accessing the context
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        console.log(room);
+        
+        if(!room) {
+            return (<div className="error">
+                <h3>oops... something went wrong :/</h3>
+                <Link to="/" className="btn-primary">
+                    return home
+                </Link>
             </div>
+            );
+        }
+        
+        
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
+
+        return (
+            <>
+            <StyledHero img = {images[0] || this.state.defaultBcg } >
+                <Banner title={`${name} room`} >
+                    <Link to="/rooms" className="btn-primary">
+                        back to rooms
+                    </Link>
+                </Banner>
+            </StyledHero>
+            </> 
         )
+            
+           
+
     }
 }
